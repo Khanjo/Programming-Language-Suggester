66ba2abc49146bcff49b0462bf6a844af515115b
Joshua Khan <Khanjo@gmail.com>
2023-06-02 14:46:17 -0700

    define variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,15 @@
 window.addEventListener("load", function (event) {
     let form = document.querySelector("form");
-    let q1 = document.querySelector(".color");
-    let q2 = document.querySelector(".climate");
-    let q3 = document.querySelector(".subject");
-    let q4 = document.querySelector(".vacation");
-    let q5 = document.querySelector(".pastime");
+
 
     form.addEventListener("submit", function (event) {
         event.preventDefault();
-        const color = parseInt(form.querySelector < q1 > ("input:radio[name=color]:checked").valueOf());
-        const climate = parseInt(form.querySelector < q2 > ("input:radio[name=climate]:checked").valueOf());
-        const subject = parseInt(form.querySelector < q3 > ("input:radio[name=subject]:checked").valueOf());
-        const vacation = parseInt(form.querySelector < q4 > ("input:radio[name=vacation]:checked").valueOf());
-        const pastime = parseInt(form.querySelector < q5 > ("input:radio[name=pastime]:checked").valueOf());
+        const color = parseInt(document.querySelector('input[name="color"]:checked').value);
+        const climate = parseInt(document.querySelector('input[name="climate"]:checked').value);
+        const subject = parseInt(document.querySelector('input[name="subject"]:checked').value);
+        const vacation = parseInt(document.querySelector('input[name="vacation"]:checked').value);
+        const pastime = parseInt(document.querySelector('input[name="pastime"]:checked').value);
 
-        return color, climate, subject, vacation, pastime
+        return color + climate + subject + vacation + pastime
     });
 });
